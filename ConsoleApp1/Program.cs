01b1b051e7bf91e8a2d1ac1ba25c609a7fc48362
shahnaz242002 <shahnazavazova@gmail.com>
2023-03-16 20:28:06 +0400

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Сколько гостей");
+int qost =Convert.ToInt32(Console.ReadLine());
+if(qost<=2)
+{
+    Console.WriteLine("Присаживайтесь к маленькому столику");
+}
+if(qost>2 && qost<=4)
+{
+    Console.WriteLine("Присаживатесь к среднему столику");
+}
+if(qost>4 && qost<=8)
+{
+    Console.WriteLine("Присаживайтесь к большому столику");
+}
+else
+{
+    Console.WriteLine("Для вас нет подходящего места");
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Пользователь должен вести пароль");
+string parol1 = Console.ReadLine();
+string parol2=Console.ReadLine();
+if(parol1==parol2)
+{
+    Console.WriteLine("регистрация прошла успешно");
+}
+else
+{
+    Console.WriteLine("регистрация не прошла успешно");
+}
