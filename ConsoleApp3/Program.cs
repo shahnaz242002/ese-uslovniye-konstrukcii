492cdb4a80c87b3f86d90c506e634c2e1c2206f6
shahnaz242002 <shahnazavazova@gmail.com>
2023-03-27 14:58:42 +0400

    добавила трее задание

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.ComponentModel.Design;
+
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1 < number2 && number2 < number3)
+{
+    Console.WriteLine($"{number3},{number2},{number1}");
+}
+else if (number3 < number2 && number2 < number1)
+{ 
+    Console.WriteLine($"{number1},{number2},{number3}");
+}
+else if (number1 < number2 && number2> number3)
+{
+    Console.WriteLine($"{number2},{number3},{number1}");
+}
+else if (number2>number1 && number1 < number3)
+{
+    Console.WriteLine($"{number2},{number1},{number3}");
+}
+else if (number2 < number3 && number3>number1)
+{
+    Console.WriteLine($"{number2},{number3},{number1}");
+}
+else
+{
+    Console.WriteLine($"{number3},{number1},{number2}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (number1 > number2 && number1 > number3)
+{
+    if (number2 > number3)
+    {
+        Console.WriteLine($"{number1},{number2},{number3}");
+    }
+    else
+    {
+        Console.WriteLine($"{number1},{number3},{number2}");
+    }
+}
+else if (number2 > number1 && number2 > number3)
+{
+    if (number1 > number3)
+    {
+        Console.WriteLine($"{number2},{number1},{number3}");
+    }
+    else
+
+    {
+        Console.WriteLine($"{number2},{number3},{number1}");
+    }
+}
+else
+ {
+    if (number1 > number2)
+    {
+        Console.WriteLine($"{number3},{number1},{number2}");
+    }
+    else
+    {
+        Console.WriteLine($"{number3},{number2},{number1}");
+    }
+}
